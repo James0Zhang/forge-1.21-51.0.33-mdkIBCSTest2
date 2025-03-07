cf93bdfbc650edd559976bf36d22cf54b756bc71
James <jameszhang9907@gmail.com>
2025-03-06 16:48:26 -0800

    Add 2 item to creative mod

--- src/main/java/com/james0z/ibcsmod/IBCSMod.java
+++ src/main/java/com/james0z/ibcsmod/IBCSMod.java
@@ -1,6 +1,8 @@
 package com.james0z.ibcsmod;
 
+import com.james0z.ibcsmod.item.ModItems;
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -29,14 +31,13 @@ public class IBCSMod
     public IBCSMod()
     {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-
-        // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
@@ -52,7 +53,12 @@ public class IBCSMod
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
+        if(event.getTabKey() == CreativeModeTabs.COMBAT){
+            event.accept(ModItems.warrior);
+            event.accept(ModItems.Ranger);}
 
+        if(event.getTabKey() == CreativeModeTabs.SPAWN_EGGS){
+            }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/com/james0z/ibcsmod/item/ModItems.java
@@ -0,0 +1,23 @@
+package com.james0z.ibcsmod.item;
+
+import com.james0z.ibcsmod.IBCSMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, IBCSMod.MOD_ID);
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+
+    public static final RegistryObject<Item> warrior = ITEMS.register("warrior",
+            ()-> new Item(new Item.Properties()));
+
+    public static final RegistryObject<Item> Ranger = ITEMS.register("ranger",
+            ()-> new Item(new Item.Properties()));
+
+}
